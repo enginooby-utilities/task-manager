ae28914f8306ed26313d3e794ef447e50bcf143a
unknown <enginoobz@gmail.com>
2021-08-12 08:04:11 +0200

    [Test] Connect & add document to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
-console.log('hi');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const dbName = 'task-manager';
+//@ts-ignore
+mongodb_1.MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error)
+        return console.log('Unable to connect to database!');
+    const db = client.db(dbName);
+    db.collection('vips').insertOne({
+        name: 'enginoobz',
+        age: 25
+    });
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('hi')
+import { MongoClient } from 'mongodb';
+
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const dbName = 'task-manager';
+//@ts-ignore
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if (error) return console.log('Unable to connect to database!')
+
+        const db = client.db(dbName)
+        db.collection('vips').insertOne({
+                name: 'enginoobz',
+                age: 25
+        })
+})
+
+
