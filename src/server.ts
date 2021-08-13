5814ccd94da6d78ff03b9f313900a35dd10f4a9b
unknown <enginoobz@gmail.com>
2021-08-13 17:04:50 +0200

    Get JSON object from POST/PUT request

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,11 @@ import { User } from './models/user'
 const app: Application = express()
 const port = process.env.PORT || 3002
 
+// to recognize the incoming POST/PUT request object as Json object, retrieved from req,body
+app.use(express.json())
+
 app.post('/users', (req, res) => {
+        console.log(req.body)
         res.send('Testing')
 })
 
